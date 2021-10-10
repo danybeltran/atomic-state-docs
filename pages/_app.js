9e30a7051d3ccc6d138e776825ca645fb6fb43b0
danybeltran <josuebeltran_2010@hotmail.com>
2021-10-09 19:16:34 -0600

    docs: Updates website description

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
 import 'nextra-theme-docs/style.css'
-
+import Head from 'next/head'
 export default function Nextra({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta
+          name="description"
+          content="Atomic State is a state managment library for React apps"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
